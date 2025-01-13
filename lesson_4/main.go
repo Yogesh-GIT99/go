553a20c379b8e6d77c2dc655b009737e182ec6c1
Yogesh Rautela <yogeshrautela101@gmail.com>
2025-01-13 08:27:27 +0530

    gretter

--- lesson_4/main.go
+++ lesson_4/main.go
@@ -1,1 +1,21 @@
+// os function / use of slices / len function / build a go binary
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fmt.Printf("%#v\n", os.Args)
+
+	fmt.Println("Path : ", os.Args[0])
+	fmt.Println("1st argument : ", os.Args[1])
+	fmt.Println("2nd argument : ", os.Args[2])
+
+	fmt.Println("No. of items inside slice value : ", len(os.Args))
+
+}
+
+// go build -o build_name ( cmd to build a go binary)
