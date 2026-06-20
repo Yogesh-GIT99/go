d912f7f3cfcb090712c7cb83509252a77065ba23
Yogesh-GIT99 <yogeshrautela101@gmail.com>
2026-06-20 13:17:48 +0530

    neetcode: twoPointer: updated readme

--- example/neetcode/twoPointer/code1/main.go
+++ example/neetcode/twoPointer/code1/main.go
@@ -1,3 +1,8 @@
+// Valid Palindrome
+// Given a string s, return true if it is a palindrome, otherwise return false.
+// A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+// Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers
+
 package main
 
 import (
